a8bdd403f4b2b4a9674c5dcfa350a47c3d99f859
Nathanael <ngreese7@ou.edu>
2020-10-28 16:33:11 -0500

    IT ALWAYS CONTINUESgit add -A!

---
+++ src/driver.java
@@ -0,0 +1,14 @@
+
+public class driver {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String[] alwaysContinues = {"A", "L", "W", "A", "Y", "S", "C", "O", "N", "T", "I", "N", "U", "E", "S"};
+		
+		for (int i = 0; i < alwaysContinues.length; ++i) {
+			System.out.println(alwaysContinues[i]);
+		}
+		System.out.println("It always continues!");
+	}
+
+}
